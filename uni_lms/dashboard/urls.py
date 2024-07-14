91fd98fee4b8046703ab46874920c783654ac2ff
arcchiebald <archiekogerge@gmail.com>
2024-07-14 04:14:59 +0400

    Added logout functionality via Django
    -also, replaced side navbar with top navbar

--- uni_lms/dashboard/urls.py
+++ uni_lms/dashboard/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "dashboard"
 
 urlpatterns = [
-    path("", views.mainpage_view, name="mainpage")
+    path("", views.mainpage_view, name="mainpage"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- uni_lms/dashboard/views.py
+++ uni_lms/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -7,3 +8,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def mainpage_view(request):
     return render(request, "mainpage.html", {})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("users:login")
